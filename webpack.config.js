7d927f62954bcae7e785e48c9e8fef9f522bbe93
Raúl Cátedra <raulcabe3003@gmail.com>
2021-01-02 21:24:44 +0100

    resolve webpack warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
+    mode: "development",
     entry: { index: path.resolve(__dirname, "./src/assets/js", "index.js") },
     output: {
         path: path.resolve(__dirname, "build"),

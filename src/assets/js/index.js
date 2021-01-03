38a29133685c204fc398b14224154e6092cbddb7
Raúl Cátedra <raulcabe3003@gmail.com>
2021-01-03 16:23:55 +0100

    display task done

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,7 +5,8 @@ import "../css/modals.css";
 import {showResponsiveMenu} from "./responsive-menu/responsive-menu.js"
 import {showModal} from './modal/show-modal.js';
 import {updateCustomLists} from './lists/list.js';
-import {updateVariableFromLocalStorage} from './local-storage/update-variable.js'
+import {updateVariableFromLocalStorage} from './local-storage/update-variable.js';
+import {updateTaskList} from './tasks/tasks.js';
 
 
 const hamburguerButton = document.getElementById('menuOpen');
@@ -14,6 +15,7 @@ const newList = document.getElementById('newList');
 
 updateVariableFromLocalStorage();
 updateCustomLists();
+updateTaskList();
 
 
 
@@ -21,8 +23,3 @@ hamburguerButton.addEventListener("click", showResponsiveMenu);
 newTask.addEventListener('click', showModal);
 newList.addEventListener('click', showModal);
 
-
-
-
-//todo: revisar todo el html y css para comprobar que los nombres estan bien y se cumple BEM
-//todo: revisar html y css para darle un estilo mas bonito a la pagina y modales en general.
--- src/assets/js/tasks/tasks.js
+++ src/assets/js/tasks/tasks.js
@@ -1,8 +1,9 @@
-export{saveNewTask, taskArray, changeValueVariableTasks}
+export{saveNewTask, taskArray, changeValueVariableTasks, updateTaskList}
 
 
 
 import { updateLocalStorage } from "../local-storage/update-local-storage";
+import { hidde } from "../modal/hide-modal";
 
 
 
@@ -10,30 +11,59 @@ let taskArray = [];
 
 function saveNewTask(e){
   e.preventDefault();
-  const taskTitle=document.getElementById('taskTitle').value;
-  const taskDescription=document.getElementById('taskDescription').value;
-  const isCompleted=document.getElementById('isCompleted').checked;
-  const isImportant=document.getElementById('isImportant').checked;
-  const selectedList=document.getElementById('selectedList').value;
-  const taskColor=document.getElementById('taskColor').value;
-  const task= new Task(taskTitle,taskDescription,isCompleted,isImportant,selectedList,taskColor);
+  const taskTitle = document.getElementById('taskTitle').value;
+  const taskDescription = document.getElementById('taskDescription').value;
+  const isCompleted = document.getElementById('isCompleted').checked;
+  const isImportant = document.getElementById('isImportant').checked;
+  const selectedList = document.getElementById('selectedList').value;
+  const taskColor = document.getElementById('taskColor').value;
+  const task = new Task(taskTitle,taskDescription,isCompleted,isImportant,selectedList,taskColor);
   taskArray.push(task);
   updateLocalStorage();
+  updateTaskList();
+  hidde('newTask');
 }
-
 
 function changeValueVariableTasks(value){
   taskArray=value;
 }
 
+function updateTaskList() {
+  const displayTask = document.getElementById('displayTask');
+  displayTask.innerHTML = '<h1 id="displayTaskTitle" class="display-task__title">Tasks:</h1>';
+  taskArray.forEach(e => {
+    const html = e.html;
+    displayTask.innerHTML += html;
+  })
+}
+
 class Task{
-  constructor(title, description, completed, important, list, color){
-    this.title=title,
-    this.description=description,
-    this.completed=completed,
-    this.important=important,
-    this.list=list,
-    this.color=color
-    //todo metodo más pintar
+  constructor(title, description, completed, important, list, color) {
+    this.title = title,
+    this.description = description,
+    this.completed = completed,
+    this.important = important,
+    this.list = list,
+    this.color = color,
+    this.html = this.createHtml()
+  }
+  createHtml() {
+    if(this.important) {
+      const html = `
+      <section class="task-item">
+        <input type="checkbox" class="task-item__button task-item__button-completed" checked="${completed}></input>
+        <p class="task-item__title ${this.color}">${this.title}</p>
+        <button class="task-item__button task-item__button-important bx bxs-flag-alt red-flag"></button>
+      </section>`
+      return html
+    } else {
+      const html = `
+      <section class="task-item">
+        <input type="checkbox" class="task-item__button task-item__button-completed" checked="${completed}></input>
+        <p class="task-item__title ${this.color}">${this.title}</p>
+        <button class="task-item__button task-item__button-important bx bxs-flag-alt"></button>
+      </section>`
+      return html
+    }
   }
 }

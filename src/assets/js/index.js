88e6e9f387320d22d9f2d4e3de6a36f4c0f3d470
Raúl Cátedra <raulcabe3003@gmail.com>
2021-01-03 13:40:00 +0100

    delete todos

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -23,11 +23,6 @@ newList.addEventListener('click', showModal);
 
 
 
-//TODO: BUENOS DIAS JOSE!!! TE DEJO UN POCO LA EXPLICACION DE POR DONDE ME HE QUEDADO CON EL PROYECTO, HAZ LO QUE TE APETEZCA, INCLUSO OTRAS FUNCIONALIDADES SI ESTAS NO TE APETECEN HACERLAS, TAMBIEN LO DEJO ESCRITO PARA ACORDARME MAÑANA DE DONDE ME HE QUEDADO QUE LA CABEZA YA NO ME DA MUCHO DE SI!! XD
 
-
-//todo: Terminar el modal de nueva tarea(se ha de cargar dinamicamente las opciones de listas creadas como opciones del select para poder añadir la tarea a esa lista directamente, esta a medio hacer);
-//Todo: Terminar validaciones de los inputs del modal de nueva tarea, hacerlo con dos variables para que cuando las dos sean true el estado del boton pase a disabled=false;
-//todo: añadir escuchador al boton save de nueva tarea para crear un objeto de una clase que se cree previamente para guardar toda la informacion de la tarea y poder filtrar luego por estas para que aprezcan o desaparezcan dinamicamente;
 //todo: revisar todo el html y css para comprobar que los nombres estan bien y se cumple BEM
 //todo: revisar html y css para darle un estilo mas bonito a la pagina y modales en general.

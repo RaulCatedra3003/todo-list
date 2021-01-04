e331a528f79069b0c7e6ebeb18ba59857a1d3bb1
jose <joseramonsr3@homtail.com>
2021-01-04 13:05:21 +0100

    add message validations

--- src/assets/js/modal/content/validations/new-list-validations.js
+++ src/assets/js/modal/content/validations/new-list-validations.js
@@ -2,10 +2,14 @@ export {validateListTitle};
 
 
 function validateListTitle(e) {
-  const leters = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  const leters = /^[0-9a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
   const newListName = e.target.value;
   const saveButton = document.getElementById('saveButton');
   if(newListName.match(leters) && newListName.length >= 3 && newListName.length <= 15) {
     saveButton.disabled = false;
-  }else{saveButton.disabled = true;}
+  }else{
+    saveButton.disabled = true;
+    e.target.value="";
+    e.target.placeholder = "Min 3 an max 15 letters";
+  }
 }
--- src/assets/js/modal/content/validations/new-task-validations.js
+++ src/assets/js/modal/content/validations/new-task-validations.js
@@ -9,25 +9,29 @@ let validationTitleAndDescription = {
 }
 
 function validateTaskDescription(e) {
-  const leters = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  const leters = /^[0-9a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
   const newTaskDescription = e.target.value;
   if (newTaskDescription.match(leters) && newTaskDescription.length >= 10 && newTaskDescription.length <= 100) {
     validationTitleAndDescription.description = true;
     enabledSaveButton();
   } else {
     validationTitleAndDescription.description = false;
+    e.target.value="";
+    e.target.placeholder = "Min 10 an max 100 letters";
     enabledSaveButton();
   }
 }
 
 function validateTaskTitle(e) {
-  const leters = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  const leters = /^[0-9a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
   const newTaskName = e.target.value;
   if (newTaskName.match(leters) && newTaskName.length >= 3 && newTaskName.length <= 25) {
     validationTitleAndDescription.title = true;
     enabledSaveButton();
   } else {
     validationTitleAndDescription.title = false;
+    e.target.value="";
+    e.target.placeholder = "Min 3 an max 25 letters";
     enabledSaveButton();
   }
 }
